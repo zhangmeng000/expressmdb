feddaf3a99dc00d9f22d0d34da6a72d282d70990
zhangmeng000 <1550685677@qq.com>
2016-09-30 11:56:37 +0800

    mongodb

---
+++ ReadMe.md
@@ -0,0 +1,24 @@
+## express
+
+1. 跑在服务器上，响应客户端发出的ｒｅｑｕｅｓｔ　决定那部分后台代码被执行
+2.学会用curl 调试ＡＰＩ
+curl --request PUT localhost:3000/posts/id
+
+***js
+app.get('/posts:id',function(req,res){
+  res.send('GET /id')//前台显示
+  console.log('GET /id')
+})
+app.put('/posts:id',function(req,res){
+  res.send('PUT /id')//前台显示
+  console.log('PUT /id')//更新
+})
+app.post('/posts/',function(req,res){
+  res.send('POST /posts')//前台显示
+  console.log('POST /posts')//上传
+})
+app.delete('/posts:id',function(req,res){
+  res.send('DELETE /id')//前台显示
+  console.log('DELETE /id')//删除
+})
+***
---
+++ index.js
@@ -0,0 +1,86 @@
+var express = require('express')
+var app = express()
+app.get('/posts',function(req,res){
+  res.send('GET /posts')//前台显示
+  console.log('GET /posts')//查找
+})
+app.get('/posts:id',function(req,res){
+  res.send('GET /id')//前台显示
+  console.log('GET /id')
+})
+app.put('/posts:id',function(req,res){
+  res.send('PUT /id')//前台显示
+  console.log('PUT /id')//更新
+})
+app.post('/posts/',function(req,res){
+  res.send('POST /posts')//前台显示
+  console.log('POST /posts')//上传
+})
+app.delete('/posts:id',function(req,res){
+  res.send('DELETE /id')//前台显示
+  console.log('DELETE /id')//删除
+})
+// app.get('/:name', function (req,res) {
+// 	var username = req.params.name;
+// 	var page = "<html>" + "<body>" + "<h2>" + username + "的购物车" + "</h2>" + "</body>" + "</html>"
+//   res.send(page)
+// })
+// app.post('/:name', function (req,res) {
+//   res.send('a Post Require revisize'+req.params.na8me)
+// })
+// app.get('/aaa', function (req, res) {
+// 	var page = "<html>" + "<body>" + "<h2>aaa.html</h2>" + "</body>" + "</html>"
+//   console.log('hello aaa')
+// })
+app.listen(3000,function(){
+	console.log('running on port 3000...please visit localhost:3000')
+})
+//跟数据库链接
+// var mongoose = require('mongoose');
+// mongoose.Promise = global.Promise;//垃圾过期文件不上传
+// mongoose.connect('mongodb://localhost:27017/express-api');
+// var db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function() {
+//   console.log('success')
+// 	var userName = mongoose.Schema({
+//     name: String,
+// 		age:Number,
+//     key:String
+// 	})
+	// var person = mongoose.model('person', userName);
+	// var zhang = new person({name:'zhangm',age:15,key:'123456'})
+	// zhang.save();
+	// var Cat = mongoose.model('cats', userName);
+	// var kitty = new Cat({name:'kitty',age:1,key:'123456'})
+  // kitty.age=2;
+	// kitty.save();
+	// var dog = mongoose.model('dogs', userName);
+	// var lim = new dog({name:'lim',age:1,key:'1234567'})
+	// lim.save();
+	// console.log(kitty.age)
+  // Cat.findById({ "_id" : "57ecbc3ccd055a0a3e5f49f3"},function(err,cat){
+  //   cat.name = 'hahaoo'
+  //   cat.save(function(err){
+  //     console.log('更新')
+  //     Cat.find().exec(function(err,cats){
+  //       console.log(cats)//异步执行
+  //     })
+  //   });
+  //   })
+  // })
+  // Cat.findById({_id :"57ecc3f74834890b1a05a5ee"},function(err,cat){
+  //   cat.remove(function(err){
+  //     console.log('删除')
+  //     Cat.find().exec(function(err,cats){
+  //       console.log(cats)//异步执行
+  //     })
+  //   });
+  //   })
+  // })
+  // console.log('我先')
+//   var resule = cat.find();
+//   cat.find().exec(function(err,cats){
+//     console.log(cats)//异步执行
+//   })
+// });
